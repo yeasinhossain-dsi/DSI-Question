d7f3ed5c2c074c9adc09256141eebd52578a00ee
Yeasin Hossain <yeasinhossain.dsi@gmail.com>
2024-06-11 00:33:33 +0600

    Fix illegal import

--- packages/api/src/main/java/com/dsi/questionBank/api/Controllers/QuestionController.java
+++ packages/api/src/main/java/com/dsi/questionBank/api/Controllers/QuestionController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.dsi.questionBank.api.Config.ErrorCode;
 import com.dsi.questionBank.api.DTO.QuestionRequest;
-import com.dsi.questionBank.api.DTO.QuestionsSimpleListResponse;
 import com.dsi.questionBank.api.Domains.Question;
 import com.dsi.questionBank.api.Domains.User;
 import com.dsi.questionBank.api.Exceptions.ValidationException;
